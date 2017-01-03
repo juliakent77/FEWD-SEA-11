3a25898170db2ffdeb2de12dcdeadab5ddb2f924
juliakent77 <juliakent@Julias-MacBook-Air.local>
2017-01-03 15:13:57 -0800

    problem with line 21, don't know why....

--- Week_05_JS_ComputerScience/Assignment/starter_code/script.js
+++ Week_05_JS_ComputerScience/Assignment/starter_code/script.js
@@ -1,19 +1,34 @@
+ 
+$(document).ready(function() {
+  // don't understand how this works!! got it from the solution code!
+
  $('#submit-btn').click(function(){
-  event.preventdefault();
+
+  event.preventDefault();
 
   var city = $('#city-type').val();
   $('#city-type').val(''); 
+  // this also confuses me!
 
   if  (city === 'new york city' || city === 'New York City' || city === 'nyc' || city === 'NYC' || city === 'new york' || city === 'New York ') {
     $('body').attr('class', 'nyc');
-  } else if (city === 'San Francisco' || city === 'san francisco' || city === 'SF' || city === 'sf' || city === 'Bay Area' || city === 'bay area') {
+  } 
+
+  else if (city === 'San Francisco' || city === 'san francisco' || city === 'SF' || city === 'sf' || city === 'Bay Area' || city === 'bay area') {
     $('body').attr('class', 'sf');
-  } else if (city === 'LA' || city === 'la' || city === 'Los Angeles' || city === 'los angeles' || city === 'LAX' || city === 'lax') {
+  } 
+
+  else if (city === 'LA' || city === 'la' || city === 'Los Angeles' || city === 'los angeles' || city === 'LAX' || city === 'lax') {
     $('body').attr('class', 'la');
-  } else if (city === 'Austin' || city === 'austin' || city === 'ATX' || city === 'atx') {
+  } 
+
+  else if (city === 'Austin' || city === 'austin' || city === 'ATX' || city === 'atx') {
     $('body').attr('class', 'austin');
-  } else (city === 'Sydney' || city === 'sydney' || city === 'SYD' || city === 'syd' || ) {
+  } 
+
+  else (city === 'Sydney' || city === 'sydney' || city === 'SYD' || city === 'syd') {
     $('body').attr('class', 'sydney');
   }
+    });
+});
 
-}
