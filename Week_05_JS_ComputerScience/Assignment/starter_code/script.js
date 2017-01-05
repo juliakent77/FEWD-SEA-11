32602e4f09cca9e6208027cd41b0aba0cc361977
juliakent77 <juliakent@Julias-MacBook-Air.local>
2017-01-04 19:30:21 -0800

    Everything works thanks to Sean

--- Week_05_JS_ComputerScience/Assignment/starter_code/script.js
+++ Week_05_JS_ComputerScience/Assignment/starter_code/script.js
@@ -1,34 +1,54 @@
  
+// $('#submit-btn').click(function(){
+
+//   event.preventDefault();
+
+//   var city = $('#city-type').val();
+//   $('#city-type').val(''); 
+//   // this also confuses me!
+
+//   if  (city === 'new york city' || city === 'New York City' || city === 'nyc' || city === 'NYC' || city === 'new york' || city === 'New York ') {
+//     $('body').attr('class', 'nyc');
+//   } 
+
+//   else if (city === 'San Francisco' || city === 'san francisco' || city === 'SF' || city === 'sf' || city === 'Bay Area' || city === 'bay area') {
+//     $('body').attr('class', 'sf');
+//   } 
+
+//   else if (city === 'LA' || city === 'la' || city === 'Los Angeles' || city === 'los angeles' || city === 'LAX' || city === 'lax') {
+//     $('body').attr('class', 'la');
+//   } 
+
+//   else if (city === 'Austin' || city === 'austin' || city === 'ATX' || city === 'atx') {
+//     $('body').attr('class', 'austin');
+//   } 
+
+//   else (city === 'Sydney' || city === 'sydney' || city === 'SYD' || city === 'syd') {
+//     $('body').attr('class', 'sydney');
+//   }
+//     });
+
 $(document).ready(function() {
-  // don't understand how this works!! got it from the solution code!
+  $('#submit-btn').click(function(event) {
+    event.preventDefault();
 
- $('#submit-btn').click(function(){
+    var city =  $('#city-type').val();
 
-  event.preventDefault();
+    city = city.toLowerCase();
+    // data normalization
 
-  var city = $('#city-type').val();
-  $('#city-type').val(''); 
-  // this also confuses me!
-
-  if  (city === 'new york city' || city === 'New York City' || city === 'nyc' || city === 'NYC' || city === 'new york' || city === 'New York ') {
-    $('body').attr('class', 'nyc');
-  } 
-
-  else if (city === 'San Francisco' || city === 'san francisco' || city === 'SF' || city === 'sf' || city === 'Bay Area' || city === 'bay area') {
-    $('body').attr('class', 'sf');
-  } 
-
-  else if (city === 'LA' || city === 'la' || city === 'Los Angeles' || city === 'los angeles' || city === 'LAX' || city === 'lax') {
-    $('body').attr('class', 'la');
-  } 
-
-  else if (city === 'Austin' || city === 'austin' || city === 'ATX' || city === 'atx') {
-    $('body').attr('class', 'austin');
-  } 
-
-  else (city === 'Sydney' || city === 'sydney' || city === 'SYD' || city === 'syd') {
-    $('body').attr('class', 'sydney');
-  }
-    });
+    if (city === 'new york city' || city === 'nyc' || city === 'new york') {
+      $('body').attr('class', 'nyc');
+    } else if (city === 'san francisco' || city === 'sf' || city === 'bay area') {
+      $('body').attr('class', 'sf');
+    } else if (city === 'la' || city === 'los angeles' || city === 'lax') {
+      $('body').attr('class', 'la');
+    } else if (city === 'austin' || city === 'atx') {
+      $('body').attr('class', 'austin');
+    } else if (city === 'sydney' || city === 'syd') {
+      $('body').attr('class', 'sydney');
+    } else {
+      alert('Hey, that is not a city!');
+    }
+  });
 });
-
